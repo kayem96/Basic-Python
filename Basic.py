048bfc6493ab3000222eff0049e083a826840562
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-08-09 21:58:49 +0600

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,8 @@
+'''
+This program will tells you that the number you giving is 0 or smaller or bigger? That means that (+ or -) number / positive or negative..?
+'''
+a = int(input('Type any intiger>>>'))
+if a < 0:
+	print('{} number is negative'.format(a))
+else:
+	print('{} number is positive'.format(a))
