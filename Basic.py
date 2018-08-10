b638b89e63cebc8a02df35f7a1437f64ad12141e
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-08-10 19:36:33 +0600

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -1,6 +1,6 @@
-'''
+print('''
 This program will tells you that the number you giving is 0 or smaller or bigger? That means that (+ or -) number / positive or negative..?
-'''
+''')
 a = int(input('Type any intiger>>>'))
 if a < 0:
 	print('{} number is negative'.format(a))
