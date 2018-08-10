1d290a59b932c3ae7a13cf79810f9cc97313ac10
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-08-11 00:57:37 +0600

    Update conditional logic and loop.py

--- conditional logic and loop.py
+++ conditional logic and loop.py
@@ -3,7 +3,7 @@ a = int(input('Type an integer >>> '))
 if (a <= 10): 
 	while (a <= 10):
 	    print(a)
-	    print('loop is Finished')
 	    a += 1
+	print('loop is finished')
 else:
 	print('your number is >>>', a)
