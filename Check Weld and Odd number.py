1bd2624660f9dd754679bb772c6afd5baaabfc1d
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-08-18 02:14:56 +0600

    Check Weld and Odd number
    This Program Says You Weld and Odd number!

---
+++ Check Weld and Odd number.py
@@ -0,0 +1,9 @@
+# Chack Weld and Odd number.
+# Programmed By Shafiul Kayem
+
+def function(number):
+	if number%2 == 0:
+		print (format(number) + ' It\'s Even Number')
+	else: print(format(number) + ' It\'s Odd Number')
+number= int(input('Enter a number to chack Weld and Odd number: '))
+function(number)
