4eb224ae2c5d3e43a4503469db71ec3be15272fd
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-08-17 10:02:58 +0600

    Seys Positive And Negative Number

--- Positive and Negative.py
+++ Positive and Negative.py
@@ -1,5 +1,5 @@
 #aks for int number than program talls you positive or negative number.
-# Programed By Shafiul Kayem.
+# Programmed By Shafiul Kayem.
 
 a = int(input('Type any intiger>>>'))
 if a < 0:
