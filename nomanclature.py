aaef9706fb5d5a1bdcd265db033717dd4ef21f6e
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-08-17 10:17:51 +0600

    seys your number nomanclature.

---
+++ nomanclature.py
@@ -0,0 +1,8 @@
+# Ask for the any number. and program tells your number nomanclature
+# Programmed By Shafiul Kayem.
+
+number = int(input('Enter your number >>> '))
+count = 1
+for b in range(1, 11):
+	print (number, '*', count, '=', number * count)
+	count += 1
