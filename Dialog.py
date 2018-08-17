420290abfadfac7edb23e2c59bf53fc02442f022
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-08-17 09:32:53 +0600

    Bots and user conversations

---
+++ Dialog.py
@@ -0,0 +1,7 @@
+# aks your name. than the bots will talk to you
+a = input ('Bot >>> Hi! What is you name..?\nUser >>> My name is : ')
+print('Bot >> Nice to Meet you', a, '☺')
+b = input ('Bot >>> What is your age..?\nUser >>> My age : ')
+print ('Bot >>> You\'re', b, 'years older than me',)
+print('Because I\'m a bot. My born in August 2018 😉 "Made by Shafiul Kayem" \nSee you later', a)
+print('Good bye ☺')
