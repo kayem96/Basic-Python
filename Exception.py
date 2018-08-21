1404a343eb6b62c63ab84a6b44ecb2f6b5d5aa1d
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-08-21 23:30:53 +0600

    Simple Exception Handling Practice

---
+++ Exception.py
@@ -0,0 +1,7 @@
+try:
+	print('Please Enter any number for divide!')
+	tmp = int(input('First Number : '))
+	temp = int(input('Second Number : '))
+	print (tmp, '/', temp, '=', tmp/temp)
+except ZeroDivisionError:
+	print (tmp, '/', temp, '=', tmp)
