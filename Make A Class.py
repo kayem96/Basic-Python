e5cd6f38ca67000f3132502612b17dc157337c31
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-09-11 22:53:11 +0600

    Try to make a class

---
+++ Make A Class.py
@@ -0,0 +1,15 @@
+# Make A Class.
+# Programmed By Shafiul Kayem.
+
+class MyCar:
+
+	def driving(self):
+		ran_the_car
+	def music(self):
+		run_the_music
+	def fuel(self):
+		load_the_fuel
+	def horn(self):
+		make_sound_pollution
+	def breaks(self):
+		Stop_the_car
