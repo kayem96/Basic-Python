7dae0cc722edc5fc8a65f4b00353e3c7a07094a1
Shafiul Kayem <shafiulkayem@gmail.com>
2018-10-15 02:03:37 +0600

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,9 @@
+"""Just Practice"""
+"""Programmed by Shafiul Kayem"""
+
+Your_name = input("What Is Your Name: ")
+print("My Name is ", Your_name)
+
+Country = "My Country Name is "
+Name = input("Enter Your Country Name: ")
+print(Country + Name)
