4c3010d337191fddb636b75b8f3631f73284059d
Shafiul Kayem <shafiulkayem@gmail.com>
2018-10-27 12:36:33 -0400

    Pop() function Folder

---
+++ pop/pop.py
@@ -0,0 +1,21 @@
+# Programmed by Shafiul Kayem!
+# Python3 program for pop() method 
+
+  
+list1 = [ 1, 2, 3, 4, 5, 6 ] 
+  
+# Pops and removes the last element from the list 
+print(list1.pop()) 
+  
+# Print list after removing last element 
+print("New List after pop : ", list1, "\n") 
+  
+list2 = [1, 2, 3, ('cat', 'bat'), 4] 
+  
+# Pop last three element 
+print(list2.pop()) 
+print(list2.pop()) 
+print(list2.pop()) 
+  
+# Print list 
+print("New List after pop : ", list2, "\n") 
