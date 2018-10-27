498ac362ce1f6ea09ac19328283c104e05307076
Shafiul Kayem <shafiulkayem@gmail.com>
2018-10-27 13:38:13 -0400

    Second

---
+++ pop/pop2.py
@@ -0,0 +1,13 @@
+
+# Python3 program showing pop() method 
+# and remaining list after each pop 
+  
+list1 = [ 1, 2, 3, 4, 5, 6 ] 
+  
+# Pops and removes the last  
+# element from the list 
+print(list1.pop(), list1) 
+  
+# Pops and removes the 0th index 
+# element from the list 
+print(list1.pop(0), list1) 
