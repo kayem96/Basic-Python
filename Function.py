035e3f404f20c048b42b697cb257c0a00a129997
shafiulkayem <shafiulkayem@gmail.com>
2018-10-28 13:12:20 -0400

    Function Basic

---
+++ Function.py
@@ -0,0 +1,7 @@
+# Function Basic
+# Write by Shafiul Kayem
+
+def Ami(Name, Home, City, Email):
+    print(Name, Home, City, Email)
+		
+Ami( "Shafiul Kayem, ","Patiya,","Chittagong,", "Shafiulkayem@gmail.com" )
