6785a65e4ae84f4cfb59cc94c52cad12fa167576
shafiulkayem <shafiulkayem@gmail.com>
2018-11-03 19:17:25 -0400

    Making Sequence using yield

---
+++ Genaretor_yield.py
@@ -0,0 +1,10 @@
+""" Genaretor Practice! Making Sequence using yeild """
+""" programmed by Shafiul Kayem """
+
+def revrange(var1):
+	while var1 >= 0:
+		yield var1
+		var1 -= 1
+
+for println in revrange(10):
+	print (println)
