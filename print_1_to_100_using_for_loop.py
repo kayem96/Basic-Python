bef3cc7039fd3aec4a291025ab6dbb6883877926
Shafiul Kayem <shafiulkayem@gmail.com>
2018-11-05 19:03:11 -0500

    print 1 to 100 usig loop

---
+++ print_1_to_100_using_for_loop.py
@@ -0,0 +1,10 @@
+# Print_1_to_100_using_for_loop!
+# Programmed by "Shafiul Kayem!"
+
+num1 = int(input("Enter any Number : "))
+num2 = 101
+for num1 in range(num1, num2):
+	if num1 <= num2:
+		print(num1)
+		num1 = num1+1
+print("Simply Programmed by Shafiul Kayem!\n")
