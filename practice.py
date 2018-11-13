14a274b13f83c09a551c57778b5ffbd0b70c8241
Shafiul Kayem <shafiulkayem@gmail.com>
2018-11-13 12:23:06 -0500

    Delete practice.py

--- practice.py
+++
@@ -1,9 +0,0 @@
-"""Just Practice"""
-"""Programmed by Shafiul Kayem"""
-
-Your_name = input("What Is Your Name: ")
-print("My Name is ", Your_name)
-
-Country = "My Country Name is "
-Name = input("Enter Your Country Name: ")
-print(Country + Name)
